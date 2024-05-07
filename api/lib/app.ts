4a92b23d5ac68af8b3ea66091263dccdbbdf03e3
Daniel Chmura <147102077+dchmurek@users.noreply.github.com>
2024-05-07 16:12:14 +0200

    Add files via upload

--- api/lib/app.ts
+++ api/lib/app.ts
@@ -1,8 +1,10 @@
-import express from 'express';
+import express, { RequestHandler, response } from 'express';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import { config } from './config'
 import Controller from 'interfaces/controller.interface';
+import mongoose, { mongo } from 'mongoose';
+import { error } from 'console';
 
 class App {
    public app: express.Application;
@@ -11,6 +13,7 @@ class App {
        this.app = express();
        this.initializeMiddlewares();
        this.initializeControllers(controllers);
+       this.connectToDatabase();
    }
 
    public initializeControllers(controllers: Controller[]): void{
@@ -22,6 +25,7 @@ class App {
    private initializeMiddlewares(): void {
         this.app.use(bodyParser.json());
         this.app.use(morgan('dev'));
+        this.app.use(this.requestLogMiddleware);
    }
 
    public listen(): void {
@@ -29,5 +33,39 @@ class App {
            console.log(`App listening on the port ${config.port}`);
        });
    }
+
+   public requestLogMiddleware: RequestHandler = (request, response, next) => {
+        console.log(`[${request.method} ${request.url} ${new Date().toISOString()}]`);
+        next();
+   }
+
+   private async connectToDatabase(): Promise<void> {
+    try {
+        await mongoose.connect(config.databaseUrl);
+        console.log('Connection with database established');
+    } catch (error) {
+        console.log('Error connecting to MongoDB: ' , error);
+    }
+
+    mongoose.connection.on('error', (error) => {
+        console.error('MongoDB connection error: ', error);
+    });
+
+    mongoose.connection.on('disconnected', () => {
+        console.log('MongoDB disconnected');
+    });
+
+    process.on('SIGINT', async () => {
+        await mongoose.connection.close();
+        console.log('MongoDB connection closed due to app termination');
+        process.exit(0);
+    });
+
+    process.on('SIGTERM', async () => {
+        await mongoose.connection.close();
+        console.log('MongoDB connection closed due to app termination');
+        process.exit(0);
+    });
+ }
 }
 export default App;
--- api/lib/index.ts
+++ api/lib/index.ts
@@ -1,8 +1,10 @@
-import DataController from './controllers/data.controller';
+import DataController from "./controllers/data.controller";
 import App from './app';
 import IndexController from './controllers/index.controller';
+import UserController from "controllers/user.controller";
 
 const app: App = new App([
+    new UserController(),
     new DataController(),
     new IndexController()
 ]);
